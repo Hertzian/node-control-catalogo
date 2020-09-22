51192c79c94cd22116ce097bc405a3ab86757b53
Lalitus <tallitosan@gmail.com>
2020-09-21 21:00:33 -0500

    modified name conceptVolumes route

---
+++ routes/conceptVolumes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const ConceptVolumesController = require('../controllers/conceptVolumes')
+const router = express.Router()
+
+// Routes
+router.get('/catalog/:catalogId/volume/:volumeId', ConceptVolumesController.getVolumeById)
+router.get('/catalog/:catalogId/concept/:conceptId', ConceptVolumesController.getVolumeByConceptId)
+router.post('/catalog/:catalogId/concept/:conceptId/material/:materialId', ConceptVolumesController.addVolumeToCatalogConceptMaterial)
+router.put('/:volumeId', ConceptVolumesController.updateVolumeToCatalogConceptMaterial)
+router.delete('/:volumeId', ConceptVolumesController.deleteVolumeFromCatalogConceptMaterial)
+
+module.exports = router
