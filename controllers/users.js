e64d597ce265a4af07099eec0910d5b6ba6d20bc
Lalitus <tallitosan@gmail.com>
2020-09-22 13:16:19 -0500

    users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,67 @@
-// this is the users controller :D
+const asyncHandler = require('../middleware/asyncHandler')
+const ErrorResponse = require('../util/errorResponse')
 const User = require('../models/User')
 
-// @desc    get users
+// @desc    get all users
 // @route   GET /api/v1/users
-// @access  private
-exports.getUsers = (req, res, next) => {
-  console.log('hello')
+// @access  private/admin
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find({}, '_id name email createdAt')
 
   res.status(200).json({
     success: true,
-    data: 'hello'
+    data: users
   })
-}
+})
+
+// @desc    get single user
+// @route   GET /api/v1/auth/users/:userId
+// @access  private/admin
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.userId)
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+
+// @desc    create user
+// @route   POST /api/v1/auth/users
+// @access  private/admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body)
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+// @desc    edit user
+// @route   PUT /api/v1/auth/users/:userId
+// @access  private/admin
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+    new: true,
+    runValidators: true
+  })
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+// @desc    delete user
+// @route   DELETE /api/v1/auth/users/:userId
+// @access  private/admin
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.userId)
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
+})
